b7cc41c6fcd9e3d65d437621c33022f97337d0fa
fedjes <salvados78@mail.ru>
2023-05-19 12:58:51 +0300

    Link

--- src/components/leftSide/NavBar.jsx
+++ src/components/leftSide/NavBar.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import s from "./NavBar.module.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <div className={s.navBar}>
             <div className={s.navBarItem}>
-                <a href="/profile">Profile</a>
+                <Link to="/profile">Profile</Link>
             </div>
             <div className={s.navBarItem}>
-                <a href="/messages">Messages</a>
+                <Link to="/messages">Messages</Link>
             </div>
             <div className={s.navBarItem}>
-                <a href="/music">Music</a>
+                <Link to="/music">Music</Link>
             </div>
             <div className={s.navBarItem}>
-                <a href="/friends">Friends</a>
+                <Link to="/friends">Friends</Link>
             </div>
-            <div className={s.navBarItem}>
+            {/* <div className={s.navBarItem}>
                 <a href="/posts">Posts</a>
+            </div> */}
+            <div className={s.navBarItem}>
+                <Link to="/news">News</Link>
             </div>
             <div className={s.navBarItem}>
-                <a href="/news">News</a>
-            </div>
-            <div className={s.navBarItem}>
-                <a href="/settings">Settings</a>
+                <Link to="/settings">Settings</Link>
             </div>
         </div>
     );
